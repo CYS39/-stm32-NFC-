6a520680c389ff6f5cb8ab3ee90ccdcd2ee9c6ee
CYS39 <2543502590@qq.com>
2022-05-16 22:10:43 +0800

    log

--- STM32工程源码/USER/stmflash.h
+++ STM32工程源码/USER/stmflash.h
@@ -15,11 +15,8 @@
  
 
 u16 STMFLASH_ReadHalfWord(u32 faddr);		  //读出半字  
-void STMFLASH_WriteLenByte(u32 WriteAddr,u32 DataToWrite,u16 Len);	//指定地址开始写入指定长度的数据
-u32 STMFLASH_ReadLenByte(u32 ReadAddr,u16 Len);						//指定地址开始读取指定长度数据
 void STMFLASH_Write(u32 WriteAddr,u16 *pBuffer,u16 NumToWrite);		//从指定地址开始写入指定长度的数据
 void STMFLASH_Read(u32 ReadAddr,u16 *pBuffer,u16 NumToRead);   		//从指定地址开始读出指定长度的数据
-
 void ErasePage(u32 WriteAddress, u8 Num);            //清除WriteAddress地址起的Num个扇区
 
 //测试写入
